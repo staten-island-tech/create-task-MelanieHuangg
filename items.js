ef75f84c012053f20298efe887539639ede1db56
MelanieHuangg <melanie.huang0306@gmail.com>
2024-01-30 10:27:29 -0500

    exotic

---
+++ items.js
@@ -0,0 +1,100 @@
+const items = [
+
+    {
+        name: "Sugar Gliders",
+        imgURL: "",
+        info: "",
+        category: "rodents",
+    },
+
+    {
+        name: "Wallaroos",
+        imgURL: "",
+        info: "",
+        category: "rodents",
+    },
+
+    {
+        name: "Coati",
+        imgURL: "",
+        info: "",
+        category: "rodents",
+    },
+
+    {
+        name: "Praying Mantises",
+        imgURL: "",
+        info: "",
+        category: "insects",
+    },
+
+    {
+        name: "Scorpions",
+        imgURL: "",
+        info: "",
+        category: "insects",
+    },
+
+    {
+        name: "Bearded Dragons",
+        imgURL: "",
+        info: "",
+        category: "reptiles",
+    },
+
+    {
+        name: "Burmese Pythons",
+        imgURL: "",
+        info: "",
+        category: "reptiles",
+    },
+
+    {
+        name: "Ornate Box Turtles",
+        imgURL: "",
+        info: "",
+        category: "reptiles",
+    },
+
+    {
+        name: "iPhone 13",
+        imgURL: "https://ss7.vzw.com/is/image/VerizonWireless/apple-iphone-13-blu-09142021?wid=930&hei=930&fmt=webp",
+        cost: "$600",
+        category: "birds",
+    },
+
+    {
+        name: "iMac",
+        imgURL: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/imac-24-no-id-blue-cto-hero-202310?wid=627&hei=566&fmt=jpeg&qlt=95&.v=1699550956870",
+        cost: "$1300",
+        category: "birds",
+    },
+
+    {
+        name: "AirPods: <br> 3rd Generación",
+        imgURL: "https://i5.walmartimages.com/asr/cd823ace-e5a4-4dbb-a98b-52425a09763d.006870179db6e5b7712d999447379a7b.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
+        cost: "$170",
+        category: "birds",
+    },
+
+    {
+        name: "AirPods: <br> 2nd Generación",
+        imgURL: "https://ss7.vzw.com/is/image/VerizonWireless/apple-airpods-with-charging-case-mv7n2am-a-iset/?wid=465&hei=465&fmt=webp",
+        cost: "$130",
+        category: "amphibians",
+    },
+
+    {
+        name: "AirPods Pro: <br> 2nd Generación",
+        imgURL: "https://i5.walmartimages.com/seo/Restored-Apple-AirPods-Pro-2-White-In-Ear-Headphones-MQD83AM-A-Refurbished_95e6f27f-61a9-4b18-903d-d724d067ce51.d0c0d62cc1b1337b847a6a2412149a26.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF",
+        cost: "$250",
+        category: "amphibians",
+    },
+
+    {
+        name: "AirPods Max",
+        imgURL: "https://ss7.vzw.com/is/image/VerizonWireless/apple-airpods-max-headphones-space-gray-mgyh3ama-a?wid=930&hei=930&fmt=webp",
+        cost: "$550",
+        category: "amphibians",
+    },
+]
--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 import './style.css'
+import './items.js'
+
+const DOMSelectors = {
+    rodents: document.getElementById("rodents-btn"),
+    insects: document.getElementById("insects-btn"),
+    reptiles: document.getElementById("reptiles-btn"),
+    birds: document.getElementById("birds-btn"),
+    amphibians: document.getElementById("amphibians-btn"),
+    container: document.getElementById("container")
+};
 
