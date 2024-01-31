ae3a60dab7b0e9f6ce383d165b9781194aa3642f
MelanieHuangg <melanie.huang0306@gmail.com>
2024-01-31 10:21:17 -0500

    exotic

--- items.js
+++ items.js
@@ -57,44 +57,44 @@ const items = [
     },
 
     {
-        name: "iPhone 13",
-        imgURL: "https://ss7.vzw.com/is/image/VerizonWireless/apple-iphone-13-blu-09142021?wid=930&hei=930&fmt=webp",
-        cost: "$600",
+        name: "Cockatoos",
+        imgURL: " ",
+        info: " ",
         category: "birds",
     },
 
     {
-        name: "iMac",
-        imgURL: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/imac-24-no-id-blue-cto-hero-202310?wid=627&hei=566&fmt=jpeg&qlt=95&.v=1699550956870",
-        cost: "$1300",
+        name: "Lories",
+        imgURL: " ",
+        info: " ",
         category: "birds",
     },
 
     {
-        name: "AirPods: <br> 3rd Generación",
-        imgURL: "https://i5.walmartimages.com/asr/cd823ace-e5a4-4dbb-a98b-52425a09763d.006870179db6e5b7712d999447379a7b.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
-        cost: "$170",
+        name: "Toucans",
+        imgURL: " ",
+        info: " ",
         category: "birds",
     },
 
     {
-        name: "AirPods: <br> 2nd Generación",
-        imgURL: "https://ss7.vzw.com/is/image/VerizonWireless/apple-airpods-with-charging-case-mv7n2am-a-iset/?wid=465&hei=465&fmt=webp",
-        cost: "$130",
+        name: "Fire-bellied Toads",
+        imgURL: " ",
+        info: " ",
         category: "amphibians",
     },
 
     {
-        name: "AirPods Pro: <br> 2nd Generación",
-        imgURL: "https://i5.walmartimages.com/seo/Restored-Apple-AirPods-Pro-2-White-In-Ear-Headphones-MQD83AM-A-Refurbished_95e6f27f-61a9-4b18-903d-d724d067ce51.d0c0d62cc1b1337b847a6a2412149a26.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF",
-        cost: "$250",
+        name: "Dwarf Clawed Frogs",
+        imgURL: " ",
+        info: " ",
         category: "amphibians",
     },
 
     {
-        name: "AirPods Max",
-        imgURL: "https://ss7.vzw.com/is/image/VerizonWireless/apple-airpods-max-headphones-space-gray-mgyh3ama-a?wid=930&hei=930&fmt=webp",
-        cost: "$550",
+        name: "Northern Leopard Frogs",
+        imgURL: " ",
+        info: " ",
         category: "amphibians",
     },
 ]
